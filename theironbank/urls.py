9801d347ca0322bc989055fe486760d8a03df53f
KiaraRaynS <sherman.rayn@outlook.com>
2016-06-20 14:00:16 -0400

    Login page added

--- theironbank/urls.py
+++ theironbank/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from appironbank.views import ViewIndex
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', ViewIndex.as_view(), name='index')
+    url(r'^$', ViewIndex.as_view(), name='index'),
+    url(r'^', include('django.contrib.auth.urls')),
 ]
