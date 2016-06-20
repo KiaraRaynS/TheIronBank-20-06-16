4c82411d4da02a52fdec099b41d3750fbae92f55
KiaraRaynS <sherman.rayn@outlook.com>
2016-06-20 14:32:22 -0400

    Registration Page added

--- appironbank/views.py
+++ appironbank/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 class ViewIndex(TemplateView):
     template_name = 'index.html'
+
+
+class SignUpView(CreateView):
+    model = User
+    form_class = UserCreationForm
--- theironbank/urls.py
+++ theironbank/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
+from django.contrib.auth.models import User
 from django.contrib import admin
-from appironbank.views import ViewIndex
+from appironbank.views import ViewIndex, SignUpView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', ViewIndex.as_view(), name='index'),
     url(r'^', include('django.contrib.auth.urls')),
+    url(r'^makeuser/$', SignUpView.as_view(model=User))
 ]
